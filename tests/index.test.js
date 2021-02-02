f5ef9ccd66ea1be072ae12764ebef089d8787c56
tsamantanis <philip.tsamantanis@icloud.com>
2021-02-02 14:25:56 -0800

    Add tests for get yr function

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,15 @@
 const  BetterDate = require('../src/')
 
-const betterDate = new BetterDate()
 const today = new Date()
+const betterDate = new BetterDate()
+const someBirthday = new BetterDate(1965, 8, 26)
 
 test('betterDate.year', function() {
     expect(betterDate.year).toBe(today.getFullYear())
+    expect(someBirthday.year).toBe(1965)
+})
+
+test('betterDate.yr', function() {
+    expect(betterDate.yr).toBe(today.getFullYear() % 100)
+    expect(someBirthday.yr).toBe(65)
 })
